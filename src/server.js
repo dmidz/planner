1118bdb7d92c11ccc1d76a39d6e78a9f07789bd9
David MARTIN <david@dmidz.com>
2020-12-18 19:29:42 +0100

    server set right port for heroku bis

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const Path = require( 'path' );
 const Hapi = require( '@hapi/hapi' );
 const Inert = require( '@hapi/inert' );
 
-console.log( '# env', process.env );
+// console.log( '# env', process.env );
 //process.env.NODE_ENV==='production'
 
 const init = async () => {
 	
 	const server = new Hapi.Server( {
-		port: ( !!process.env.HEROKU ) ? 443 : 3000,
+		port: ( process.env.NODE_ENV==='production' || !!process.env.HEROKU ) ? 80 : 3000,
 		routes: {
 			files: {
 				relativeTo: Path.join( __dirname, '../dist' )
