681b104927c9b11903f407bf5cad8d312e47ae20
David MARTIN <david@dmidz.com>
2020-12-18 19:11:21 +0100

    server set right port for heroku

--- src/server.js
+++ src/server.js
@@ -1,34 +1,37 @@
-const Path = require('path');
-const Hapi = require('@hapi/hapi');
-const Inert = require('@hapi/inert');
+const Path = require( 'path' );
+const Hapi = require( '@hapi/hapi' );
+const Inert = require( '@hapi/inert' );
+
+console.log( '# env', process.env );
+//process.env.NODE_ENV==='production'
 
 const init = async () => {
-
-    const server = new Hapi.Server({
-        port: 3000,
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, '../dist')
-            }
-        }
-    });
-
-    await server.register(Inert);
-
-    server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: '.',
-                redirectToSlash: true
-            }
-        }
-    });
-
-    await server.start();
-
-    console.log('Server running at:', server.info.uri);
+	
+	const server = new Hapi.Server( {
+		port: ( !!process.env.HEROKU ) ? 443 : 3000,
+		routes: {
+			files: {
+				relativeTo: Path.join( __dirname, '../dist' )
+			}
+		}
+	} );
+	
+	await server.register( Inert );
+	
+	server.route( {
+		method: 'GET',
+		path: '/{param*}',
+		handler: {
+			directory: {
+				path: '.',
+				redirectToSlash: true
+			}
+		}
+	} );
+	
+	await server.start();
+	
+	console.log( 'Server running at:', server.info.uri );
 };
 
 init();
